e67b466468143eedacfb1f8a9128965597726ba6
ogan keskiner <ogankeskiner@gmail.com>
2016-06-04 18:12:25 +0300

    Add project files.

---
+++ MongoSampleDotNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MongoSampleDotNet.Startup))]
+namespace MongoSampleDotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
